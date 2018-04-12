1302c69fc2939963ae7f2e5558ccd173b512f1ed
RyanLPrince <37381046+RyanLPrince@users.noreply.github.com>
2018-04-12 14:28:42 +0100

    fizz buzz completed -additional

---
+++ QA-Exercises/QA-JS/Iterations2-FizzBuzz.js
@@ -0,0 +1,18 @@
+function printList(){
+    for (var i=1;i<101;i++){
+        if (i%3==0&&i%5==0){
+            document.write("FizzBuzz"+"\t\t");
+        }
+        else if (i%5==0){
+            document.write("Buzz"+"\t\t");
+        }
+        else if(i%3==0){
+            document.write("Fizz"+"\t\t");
+        }
+        
+        else{
+            document.write(i+"\t\t");
+        }
+        
+    }
+}
