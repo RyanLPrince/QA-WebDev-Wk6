dd9bcfdb6160cbd8ec23bc661409157850ae0f57
RyanLPrince <37381046+RyanLPrince@users.noreply.github.com>
2018-04-13 10:38:04 +0100

    DOM1 working minus delete element

--- QA-Exercises/QA-JS/Dom1.js
+++ QA-Exercises/QA-JS/Dom1.js
@@ -1,12 +1,21 @@
 function createPara(t){
     var para=document.createElement("p");
-    //para.setAttribute("id","paragraph");
+    para.setAttribute("id","paragraphID")
     var text=document.createTextNode(t)
     para.appendChild(text);
-    document.body.appendChild(para);
+    var element=document.getElementById("newContent");
+    element.appendChild(para);
+    //document.body.appendChild(text);
 }
 function writePara(){
     var t=document.getElementById("paragraph").value;
     createPara(t);
 }
 
+function editPara(){
+    var nt=document.getElementById("newParagraph").value;
+    //var temp=document.getElementById("paragraphID").id;
+    //document.getElementById("paragraphID").innerHTML=nt;
+    document.getElementById("newContent").innerHTML=nt;
+    
+}
